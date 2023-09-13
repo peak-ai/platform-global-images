5559d594723e5a3549d2ff714b99a4af0381b277
peak-jiteshahuja <jitesh.ahuja@peak.ai>
2023-09-13 12:18:55 +0530

    fix: try installing vscode-database separately

--- workspace/python/ds-pack/3.0.0-base-python-3.8.17/codeserver-proxy/jupyter_codeserver_proxy/__init__.py
+++ workspace/python/ds-pack/3.0.0-base-python-3.8.17/codeserver-proxy/jupyter_codeserver_proxy/__init__.py
@@ -6,9 +6,7 @@ def setup_codeserver():
                 '--auth', 
                 'none', 
                 '--disable-telemetry', 
-                '--port={port}',
-                '--install-extension',
-                'cweijan.vscode-database-client2'],
+                '--port={port}'],
     'launcher_entry': {
         'title': 'VS Code',
         'icon_path': os.path.join(os.path.dirname(os.path.abspath(__file__)), 'icons', 'vs_code_icon.svg')
