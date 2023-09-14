8ab51c00fd02174152d794554f0e9f28184a74f5
peak-jiteshahuja <jitesh.ahuja@peak.ai>
2023-09-14 23:36:36 +0530

    fix: try moving code-server plugin installation below jupyterhub

--- workspace/python/ds-pack/2.1.0-base-python-3.8.18/setup_notebook.sh
+++ workspace/python/ds-pack/2.1.0-base-python-3.8.18/setup_notebook.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-code-server --install-extension cweijan.vscode-database-client2@6.6.3
 jupyterhub --Spawner.default_url=/lab --Spawner.http_timeout=90 --NotebookApp.terminado_settings={"shell_command":"[/bin/bash]"}
+code-server --install-extension cweijan.vscode-database-client2@6.6.3
