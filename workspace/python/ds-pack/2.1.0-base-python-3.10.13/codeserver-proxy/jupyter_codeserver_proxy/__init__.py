b6515f15c256f682336d9857e9a1cf85a71a0ae5
peak-jiteshahuja <jitesh.ahuja@peak.ai>
2023-09-14 18:29:49 +0530

    fix: try adding install extension to setup_codeserver

--- workspace/python/ds-pack/2.1.0-base-python-3.10.13/codeserver-proxy/jupyter_codeserver_proxy/__init__.py
+++ workspace/python/ds-pack/2.1.0-base-python-3.10.13/codeserver-proxy/jupyter_codeserver_proxy/__init__.py
@@ -6,7 +6,8 @@ def setup_codeserver():
                 '--auth', 
                 'none', 
                 '--disable-telemetry', 
-                '--port={port}'],
+                '--port={port}',
+                '--install-extension cweijan.vscode-database-client2@6.6.3'],
     'launcher_entry': {
         'title': 'VS Code',
         'icon_path': os.path.join(os.path.dirname(os.path.abspath(__file__)), 'icons', 'vs_code_icon.svg')
