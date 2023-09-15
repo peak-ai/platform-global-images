8e19554edbb3e7809e3d420aafe41b22d97ed47d
peak-jiteshahuja <jitesh.ahuja@peak.ai>
2023-09-15 13:02:31 +0530

    refactor: remove unwanted code

--- workspace/python/ds-pack/2.1.0-base-python-3.8.18/setup_notebook.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-jupyterhub --Spawner.default_url=/lab --Spawner.http_timeout=90 --NotebookApp.terminado_settings={"shell_command":"[/bin/bash]"}
-code-server --install-extension cweijan.vscode-database-client2@6.6.3
